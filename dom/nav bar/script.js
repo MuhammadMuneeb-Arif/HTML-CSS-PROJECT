2827cf4872e764db34bae78c5c4cef27b7ba65cd
M.MUNEEB AHMED <161030327+MuhammadMuneeb-Arif@users.noreply.github.com>
2024-09-27 21:09:50 +0500

    Add files via upload

---
+++ dom/nav bar/script.js
@@ -0,0 +1,6 @@
+"use strict";
+const mobileMenuButton = document.querySelector('#mobile-menu-button');
+const mobileMenu = document.querySelector('.mobile-menu');
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
